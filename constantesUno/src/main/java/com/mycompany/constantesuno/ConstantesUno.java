9578b69e2a804a28967c8dde891f0a39419e720b
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:45:17 -0500

    Add files via upload

---
+++ constantesUno/src/main/java/com/mycompany/constantesuno/ConstantesUno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.constantesuno;
+
+/**
+ *
+ * @author john
+ */
+public class ConstantesUno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
